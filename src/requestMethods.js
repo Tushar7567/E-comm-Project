a3598230af0c758db04355b2f4630af6d4b47b42
Tushar7567 <swain.tushar00@gmail.com>
2022-12-25 12:35:05 +0530

    h

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const BASE_URL = "https://e-comm-backend-meks.onrender.com/api";
 
 
-// const user = JSON.parse(localStorage.getItem("userToken"));
-// const TOKEN = user[1]?.accessToken;
+const user = JSON.parse(localStorage.getItem("userToken")) ? JSON.parse(localStorage.getItem("userToken")) : [0,{accessToken: 1}] ;
+const TOKEN = user[1]?.accessToken;
 // console.log(TOKEN)
 
 
@@ -15,5 +15,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${JSON.parse(localStorage.getItem("userToken"))[1].accessToken}` },
+  header: { token: `Bearer ${TOKEN}` },
 });
